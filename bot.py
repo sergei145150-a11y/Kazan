cb0300e04e306dad2cecf345e06e0812fd775a47
sergei145150-a11y <sergei145150@gmail.com>
2026-04-27 15:01:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -192,34 +192,33 @@ for event in longpoll.listen():
         low = msg.lower()
 
         if user_id in states:
+            step = states[user_id]["step"]
 
-    step = states[user_id]["step"]
-
-        if step == "set_uid":
+            if step == "set_uid":
         states[user_id]["uid"] = msg
         states[user_id]["step"] = "set_field"
         send(user_id, "Введите поле:")
         continue
 
-        elif step == "set_field":
+            elif step == "set_field":
         states[user_id]["field"] = msg
         states[user_id]["step"] = "set_value"
         send(user_id, "Введите значение:")
         continue
 
-        elif step == "set_value":
+            elif step == "set_value":
         uid = states[user_id]["uid"]
         field = states[user_id]["field"]
 
-        if uid in mods:
+                if uid in mods:
             mods[uid][field] = msg
             save_data()
             send(user_id, "✅ Данные изменены.", keyboard=admin_keyboard())
-        else:
+                else:
             send(user_id, "❌ Не найден.")
 
-        del states[user_id]
-        continue
+            del states[user_id]
+            continue
 
         # ===================
         # BUTTONS
